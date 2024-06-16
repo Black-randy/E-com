35f8000dd7ee3c5dc4250a00dd7697f6416cdeb9
Black-randy <asiri.2704gg@gmail.com>
2024-06-16 15:30:23 +0530

    handle singin removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from 'react';
 import './App.css';
 import Login from './components/login/Login';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {app} from './firebase/Firebase';
+
+// Initialize Firebase App if not already initialized
+if (!app) {
+  initializeApp(app);
+}
+
+// Initialize Firebase Authentication and get a reference to the service
+getAuth(app);
 
 function App() {
   return (
     <div className="App">
-   
       <Login/>
     </div>
   );
 }
 
 export default App;
+
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -11,20 +11,36 @@ import {
   MDBCheckbox,
   MDBIcon
 }
-from 'mdb-react-ui-kit';
+  from 'mdb-react-ui-kit';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 
 function Login() {
+  const auth = getAuth();
+signInWithEmailAndPassword(auth, 'admin@amail.com', 'admin@amail.com')
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
   return (
-    <MDBContainer fluid className='bg-image'>
-        
+    <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
+
       <MDBRow>
+
         <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
-          <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{color: 'hsl(218, 81%, 95%)'}}>
+
+          <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{ color: 'hsl(218, 81%, 95%)' }}>
             The best offer <br />
-            <span style={{color: 'hsl(218, 81%, 75%)'}}>for your business</span>
+            <span style={{ color: 'hsl(218, 81%, 75%)' }}>for your business</span>
           </h1>
 
-          <p className='px-3' style={{color: 'hsl(218, 81%, 85%)'}}>
+          <p className='px-3' style={{ color: 'hsl(218, 81%, 85%)' }}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Eveniet, itaque accusantium odio, soluta, corrupti aliquam
             quibusdam tempora at cupiditate quis eum maiores libero
@@ -35,47 +51,46 @@ function Login() {
 
         <MDBCol md='6' className='position-relative'>
 
-          
+
           <MDBCard className='my-5 bg-glass'>
             <MDBCardBody className='p-5'>
 
               <MDBRow>
                 <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text' />
                 </MDBCol>
 
                 <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text' />
                 </MDBCol>
               </MDBRow>
 
-              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email'/>
-              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password'/>
+              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email' />
+              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password' />
 
               <div className='d-flex justify-content-center mb-4'>
                 <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
               </div>
 
-              <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
 
               <div className="text-center">
 
                 <p>or sign up with:</p>
 
                 <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='facebook-f' size="sm"/>
+                  <MDBIcon fab icon='facebook-f' size="sm" />
                 </MDBBtn>
 
                 <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='twitter' size="sm"/>
+                  <MDBIcon fab icon='twitter' size="sm" />
                 </MDBBtn>
 
                 <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='google' size="sm"/>
+                  <MDBIcon fab icon='google' size="sm" />
                 </MDBBtn>
 
                 <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='github' size="sm"/>
+                  <MDBIcon fab icon='github' size="sm" />
                 </MDBBtn>
 
               </div>
